771b9ab5da01cb2a75fff4ed51ae751ad978b674
mohamed.dahani.dev <mohamed.dahani.dev@gmail.com>
2024-01-25 18:33:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentInput = "";
+
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById("display").value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        document.getElementById("display").value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+        currentInput = "";
+    }
+}
+function deleteDisplay() {
+    currentInput = currentInput.slice(0, -1)
+    document.getElementById("display").value = currentInput;
+}
